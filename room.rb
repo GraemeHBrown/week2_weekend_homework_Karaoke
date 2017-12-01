dcdca9affb7c8649654e63814f63b92d964ba4e1
Graeme Brown <graemehbrown@googlemail.com>
2017-12-01 17:41:09 +0000

    Added guests and songs properties to Room to hold room guests and song playlist.

--- room.rb
+++ room.rb
@@ -1,12 +1,12 @@
 class Room
 
-attr_reader :name, :capacity
+attr_reader :name, :capacity, :guests, :songs
 
-def initialize(name, capacity, guests, songs)
+def initialize(name, capacity)
   @name = name
   @capacity = capacity
-  @guests = guests || []
-  @songs = songs || []
+  @guests = []
+  @songs = []
 end
 
 
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -5,7 +5,7 @@ require_relative('../room')
 class TestRoom < MiniTest::Test
 
   def setup
-    @room = Room.new('Room 1', 20, [], [])
+    @room = Room.new('Room 1', 20)
   end
 
   def test_room_has_name()
@@ -15,5 +15,15 @@ class TestRoom < MiniTest::Test
   def test_room_capacity()
     assert_equal(20, @room.capacity())
   end
+
+  def test_room_starts_empty()
+    assert_equal(0, @room.guests().size())
+  end
+
+  def test_room_has_no_songs_at_start()
+    assert_equal(0, @room.songs().size())
+  end
+
+
 
 end
