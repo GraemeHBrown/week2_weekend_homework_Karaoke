5f9e03a3d94dfcfc9f58aaaaa23d425f9097740d
Graeme Brown <graemehbrown@googlemail.com>
2017-12-01 18:03:34 +0000

    Added check_in method to Room class to check_in guests.  Added a guest_count method to Room to return number of guests in room.

--- room.rb
+++ room.rb
@@ -9,6 +9,14 @@ def initialize(name, capacity)
   @songs = []
 end
 
+def guest_count()
+  return @guests.size()
+end
+
+def check_in(guest)
+  @guests.push(guest)
+end
+
 
 
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -1,11 +1,13 @@
 require('minitest/autorun')
 require('minitest/rg')
 require_relative('../room')
+require_relative('../guest')
 
 class TestRoom < MiniTest::Test
 
   def setup
     @room = Room.new('Room 1', 20)
+    @guest1 = Guest.new('Fred', 15.00)
   end
 
   def test_room_has_name()
@@ -17,11 +19,28 @@ class TestRoom < MiniTest::Test
   end
 
   def test_room_starts_empty()
-    assert_equal(0, @room.guests().size())
+    assert_equal(0, @room.guest_count())
   end
 
   def test_room_has_no_songs_at_start()
     assert_equal(0, @room.songs().size())
+  end
+
+  def test_check_in__increases_guest_count()
+    before_guest_count = @room.guest_count
+    @room.check_in(@guest1)
+    count_after_addition = @room.guest_count
+    assert_equal(before_guest_count+1, count_after_addition)
+  end
+
+  def test_check_in__guests_contains_guest_added()
+    assert_equal(false, @room.guests().include?(@guest1))
+    @room.check_in(@guest1)
+    assert_equal(true, @room.guests().include?(@guest1))
+  end
+
+  def test_guest_count__empty_room()
+    assert_equal(0, @room.guest_count())
   end
 
 
