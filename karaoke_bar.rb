9338b4492f2ca4a07436e7c18481f81f1a2dfc57
Graeme Brown <graemehbrown@googlemail.com>
2017-12-02 19:02:43 +0000

    Added rooms property to KaraokeBar class to hold room objects.  Added number_of_rooms property toshow venue size.

--- karaoke_bar.rb
+++ karaoke_bar.rb
@@ -1,12 +1,12 @@
 class KaraokeBar
 
-attr_reader :name
+attr_reader :name, :rooms, :number_of_rooms
 
   def initialize(name, rooms, entry_fee, number_of_rooms)
     @name = name
     @rooms = rooms || []
-    @entry_fee = entry_fee
-    @number_of_rooms = number_of_rooms
+    @entry_fee = entry_fee.to_f
+    @number_of_rooms = number_of_rooms.to_i
   end
 
 end
--- specs/karaoke_bar__spec.rb
+++ specs/karaoke_bar__spec.rb
@@ -1,20 +1,30 @@
 require('minitest/autorun')
 require('minitest/rg')
 require_relative('../room')
-require_relative('../guest')
-require_relative('../song')
 require_relative('../karaoke_bar')
 
 class TestKaraokeBar < MiniTest::Test
 
+ENTRY_FEE = 15.00
+
   def setup
-    @bar = KaraokeBar.new('Sing Sing!', [], 15.00, 5)
+    @room1 = Room.new('Room 1', 20)
+    @room2 = Room.new('Room 2', 20)
+    @room3 = Room.new('Room 3', 20)
+    @room4 = Room.new('Room 4', 20)
+    @room4 = Room.new('Room 5', 20)
+    @rooms = [@room1, @room2, @room3, @room4, @room5]
+    @bar = KaraokeBar.new('Sing Sing!', @rooms, 15.00, 5)
   end
 
 def test_get_name()
   assert_equal('Sing Sing!', @bar.name())
-
 end
+
+def test_bar_has_rooms()
+  assert_equal(@bar.number_of_rooms, @bar.rooms().size())
+end
+
 
 
 end
